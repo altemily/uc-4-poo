e27159b2b74fc4e69723b7c1832e26c74da5cc5c
ARIANE EMILY BASILIO DE CARVALHO <a95421668@rn.senac.local>
2024-10-30 17:36:21 -0300

    Update: Arquivo da aula 2.

---
+++ aula-02/atividade/conta.js
@@ -0,0 +1,25 @@
+class Conta{
+    #saldo;
+
+    constructor(saldoInicial){
+        this.#saldo = saldoInicial
+    }
+    depositar(valor){
+        if(valor>0){
+            this.#saldo += valor;
+        }
+    }
+    sacar(valor){
+            if(valor > 0 && valor<=this.#saldo)
+                this.#saldo -= valor;
+    }
+    get getSaldo(){
+        return this.saldo;
+    }
+
+}
+
+const c1 = new Conta(500);
+console.log(c1.getSaldo);
+c1.saldo = 5000
+console.log(c1.saldo);
